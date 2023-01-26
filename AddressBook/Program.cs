4cbd8b825e934e199b5b118aca19e3c1e93e2f6a
harsh <harshjain112001@gmail.com>
2023-01-26 21:01:23 +0530

    added sort by city,state,zip

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -55,21 +55,148 @@ namespace AddressBook
             Console.WriteLine($"contact-> first name : {this.firstName} last name : {this.lastName} address : {this.address} city : {this.city} state : {this.state} zipcode : {this.zipcode} phonenumber : {this.phoneNumber} email : {this.email}");
         }
     }
+    public static class AddressBookMap
+    {
+        public static Dictionary<string,AddressBook> map=new Dictionary<string, AddressBook>();
+        public static Dictionary<string,List<Contact>> citypersonmap= new Dictionary<string, List<Contact>>();
+        public static Dictionary<string, List<Contact>> statepersonmap = new Dictionary<string, List<Contact>>();
+        public static void Allpersoninacityacrossalladdressbooks(string city)
+        {
+            foreach(var person in citypersonmap[city])
+            {
+                person.print();
+            }
+        }
+
+        public static void Allpersoninastateacrossalladdressbooks(string state)
+        {
+            foreach (var person in statepersonmap[state])
+            {
+                person.print();
+            }
+        }
+        public static int countAllpersoninacityacrossalladdressbooks(string city)
+        {
+            return citypersonmap[city].Count;
+        }
+        public static int countAllpersoninastateacrossalladdressbooks(string state)
+        {
+            return statepersonmap[state].Count;
+        }
+    }
     public class AddressBook
     {
-        private List<Contact> contactList;
+        //private List<Contact> contactList;
         private Dictionary<string, Contact> contactDictionary;
+        private Dictionary<string, List<Contact>> contacttocityDictionary;
+        private Dictionary<string, List<Contact>> contacttostateDictionary;
+        public string name;
 
-        public AddressBook()
+        public void sortbyname()
+        {
+            foreach (KeyValuePair<string, Contact> contact in contactDictionary.OrderBy(key => key.Value.firstName))
+            {
+                contact.Value.print();
+            }
+        }
+        public void sortbycity()
+        {
+            foreach (KeyValuePair<string, Contact> contact in contactDictionary.OrderBy(key => key.Value.city))
+            {
+                contact.Value.print();
+            }
+        }
+        public void sortbystate()
+        {
+            foreach (KeyValuePair<string, Contact> contact in contactDictionary.OrderBy(key => key.Value.state))
+            {
+                contact.Value.print();
+            }
+        }
+        public void sortbyzip()
+        {
+            foreach (KeyValuePair<string, Contact> contact in contactDictionary.OrderBy(key => key.Value.zipcode))
+            {
+                contact.Value.print();
+            }
+        }
+        public AddressBook(string name)
         {
             //contactList = new List<Contact>();
             contactDictionary = new Dictionary<string, Contact>();
+            contacttostateDictionary= new Dictionary<string, List<Contact>>();
+            contacttocityDictionary= new Dictionary<string, List<Contact>>();
+            this.name = name;
+            AddressBookMap.map[name]= this;
         }
         public void addContact(string firstName, string lastName, string address, string city, string state, string zipcode, string phoneNumber, string email)
         {
             Contact detail = new Contact(firstName,lastName,address,city,state,zipcode,phoneNumber,email);
             //contactList.Add(detail);
+            if (contactDictionary.ContainsKey(firstName))
+            {
+                Console.WriteLine($"already a contact with {firstName} exists");
+                return;
+            }
             contactDictionary[firstName] = detail;
+            if(contacttocityDictionary.ContainsKey(detail.city)) 
+            {
+                contacttocityDictionary[detail.city].Add(detail);
+            }
+            else
+            {
+                contacttocityDictionary[detail.city] = new List<Contact>();
+                contacttocityDictionary[detail.city].Add(detail);
+            }
+            if (contacttostateDictionary.ContainsKey(detail.state))
+            {
+                contacttostateDictionary[detail.state].Add(detail);
+            }
+            else
+            {
+                contacttostateDictionary[detail.state] = new List<Contact>();
+                contacttostateDictionary[detail.state].Add(detail);
+            }
+            if (AddressBookMap.citypersonmap.ContainsKey(detail.city))
+            {
+                AddressBookMap.citypersonmap[detail.city].Add(detail);
+            }
+            else
+            {
+                AddressBookMap.citypersonmap[detail.city]=new List<Contact>();
+                AddressBookMap.citypersonmap[detail.city].Add(detail);
+            }
+            if (AddressBookMap.statepersonmap.ContainsKey(detail.state))
+            {
+                AddressBookMap.statepersonmap[detail.state].Add(detail);
+            }
+            else
+            {
+                AddressBookMap.statepersonmap[detail.state]=new List<Contact>();
+                AddressBookMap.statepersonmap[detail.state].Add(detail);
+            }
+        }
+        public void contatctinparticularcityinthisaddressbook(string city) 
+        {
+            foreach(var person in contacttocityDictionary[city]) 
+            {
+                person.print();
+            }
+        }
+        public void contatctinparticularstateinthisaddressbook(string state)
+        {
+            foreach (var person in contacttostateDictionary[state])
+            {
+                person.print();
+            }
+        }
+        public int countAllpersoninacityacrossalladdressbooks(string city)
+        {
+            return contacttocityDictionary[city].Count;
+        }
+        public int countcontatctinparticularstateinthisaddressbook(string state)
+        {
+            return contacttostateDictionary[state].Count;
         }
         public void editContact(string firstName)
         {
@@ -155,15 +282,31 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, this is addressbook");
-            AddressBook contactlist= new AddressBook();
-            contactlist.addContact("harsh","jain","abdnagar","sgr","mp","47983","823879312","hars@.com");
-            contactlist.printAllContacts();
-            contactlist.editContact("harsh");
-            contactlist.addContact("sparsh", "pakaain", "abdgarh", "blr", "karnataka", "462283", "275163312", "sparars@.com");
-            contactlist.printAllContacts();
-            contactlist.deleteContact("harsh");
-            Console.WriteLine("after deletion");
-            contactlist.printAllContacts();
+            AddressBook contactlist= new AddressBook("contactlist");
+            contactlist.addContact("harsh","jain","h.no.-100","sagar","mp","47983","823879312","hars@.com");
+            //contactlist.printAllContacts();
+            //contactlist.editContact("harsh");
+            contactlist.addContact("sparsh", "palak", "park avenue street", "bangalore", "karnataka", "462283", "275163312", "sparars@.com");
+            contactlist.addContact("Aarsh", "palak", "park avenue street", "bangalore", "karnataka", "462283", "275163312", "sparars@.com");
+            //contactlist.printAllContacts();
+            Console.WriteLine("After Sorting by name ");
+            contactlist.sortbyname();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("After Sorting by city ");
+            contactlist.sortbycity();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("After Sorting by zip ");
+            contactlist.sortbyzip();
+            Console.WriteLine(Environment.NewLine);
+            //contactlist.printAllContacts();
+            //contactlist.deleteContact("harsh");
+            //Console.WriteLine("after deletion");
+            //contactlist.printAllContacts();
+            //AddressBookMap.Allpersoninacityacrossalladdressbooks("sagar");
+            //Console.WriteLine($"total people from mp are{AddressBookMap.countAllpersoninastateacrossalladdressbooks("mp")}");
+            //AddressBookMap.Allpersoninastateacrossalladdressbooks("karnataka");
+            //Console.WriteLine("for particular addressbook");
+            //contactlist.contatctinparticularstateinthisaddressbook("mp");
         }
     }
 }
